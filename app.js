7361d66467c1161599ae2696488d9956dd207e7a
Christopher Lopes <christopher.lopes@gmail.com>
2019-01-18 21:25:21 -0800

    Add more books and style with sticky header, colors, divider

--- app.js
+++ app.js
@@ -20,14 +20,74 @@ var totalChapters = 1189;
 
 var books = {
     genesis: 0,
-    exodus: 0
+    exodus: 0,
+    leviticus: 0,
+    numbers: 0,
+    deuteronomy: 0,
+    joshua: 0,
+    judges: 0,
+    ruth: 0,
+    firstsamuel: 0,
+    secondsamuel: 0,
+    firstkings: 0,
+    secondkings: 0,
+    firstchronicles: 0,
+    secondchronicles: 0,
+    ezra: 0,
+    nehemiah: 0,
+    esther: 0,
+    job: 0,
+    psalms: 0,
+    proverbs: 0,
+    ecclesiastes: 0,
+    song: 0,
+    isaiah: 0,
+    jeremiah: 0,
+    lamentations: 0,
+    ezekiel: 0,
+    daniel: 0,
+    hosea: 0,
+    joel: 0,
+    amos: 0,
+    obadiah: 0,
+    jonah: 0,
+    micah: 0,
+    nahum: 0,
+    habakkuk: 0,
+    zephaniah: 0,
+    haggai: 0,
+    zechariah: 0,
+    malachi: 0,
+    matthew: 0,
+    mark: 0,
+    luke: 0,
+    john: 0,
+    acts: 0,
+    romans: 0,
+    firstcorinthians: 0,
+    secondcorinthians: 0,
+    galatians: 0,
+    ephesians: 0,
+    philippians: 0,
+    colossians: 0,
+    firstthessalonians: 0,
+    secondthessalonians: 0,
+    firsttimothy: 0,
+    secondtimothy: 0,
+    titus: 0,
+    philemon: 0,
+    hebrews: 0,
+    james: 0,
+    firstpeter: 0,
+    secondpeter: 0,
+    firstjohn: 0,
+    secondjohn: 0,
+    thirdjohn: 0,
+    jude: 0,
+    revelation: 0
 };
 
-function displayBooks() {}
-
-// displayBooks();
-
-function test(book, chapters) {
+function calculateBookPercentage(book, chapters) {
     var count = 0;
     document.querySelectorAll('#' + book + ' input[type=checkbox]').forEach(function(checkbox) {
         if (checkbox.checked) {
