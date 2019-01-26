5c4a361ead00b5452ef2698d8f59ee455ed8af7b
Christopher Lopes <christopher.lopes@gmail.com>
2019-01-26 14:53:41 -0800

    Store the completed chapters in indexDb

--- app.js
+++ app.js
@@ -16,91 +16,125 @@ if ('serviceWorker' in navigator) {
         });
 }
 
-var totalChapters = 1189;
+var books = {};
 
-var books = {
-    genesis: 0,
-    exodus: 0,
-    leviticus: 0,
-    numbers: 0,
-    deuteronomy: 0,
-    joshua: 0,
-    judges: 0,
-    ruth: 0,
-    firstsamuel: 0,
-    secondsamuel: 0,
-    firstkings: 0,
-    secondkings: 0,
-    firstchronicles: 0,
-    secondchronicles: 0,
-    ezra: 0,
-    nehemiah: 0,
-    esther: 0,
-    job: 0,
-    psalms: 0,
-    proverbs: 0,
-    ecclesiastes: 0,
-    song: 0,
-    isaiah: 0,
-    jeremiah: 0,
-    lamentations: 0,
-    ezekiel: 0,
-    daniel: 0,
-    hosea: 0,
-    joel: 0,
-    amos: 0,
-    obadiah: 0,
-    jonah: 0,
-    micah: 0,
-    nahum: 0,
-    habakkuk: 0,
-    zephaniah: 0,
-    haggai: 0,
-    zechariah: 0,
-    malachi: 0,
-    matthew: 0,
-    mark: 0,
-    luke: 0,
-    john: 0,
-    acts: 0,
-    romans: 0,
-    firstcorinthians: 0,
-    secondcorinthians: 0,
-    galatians: 0,
-    ephesians: 0,
-    philippians: 0,
-    colossians: 0,
-    firstthessalonians: 0,
-    secondthessalonians: 0,
-    firsttimothy: 0,
-    secondtimothy: 0,
-    titus: 0,
-    philemon: 0,
-    hebrews: 0,
-    james: 0,
-    firstpeter: 0,
-    secondpeter: 0,
-    firstjohn: 0,
-    secondjohn: 0,
-    thirdjohn: 0,
-    jude: 0,
-    revelation: 0
-};
+idbKeyval.get('books').then(function(cacheBooks) {
+    books = cacheBooks ? cacheBooks : initBooks();
 
-function calculateBookPercentage(book, chapters) {
+    for (var book in books) {
+        var count = 0;
+        var checkboxes = document.querySelectorAll('#' + book + ' input[type=checkbox]');
+        for (var i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].checked = books[book][i];
+            if (checkboxes[i].checked) {
+                count++;
+            }
+        }
+        setBookPercentage(book, count);
+    }
+
+    setBiblePercentage();
+});
+
+function calculateBookPercentage(book) {
     var count = 0;
-    document.querySelectorAll('#' + book + ' input[type=checkbox]').forEach(function(checkbox) {
-        if (checkbox.checked) {
+    var a = document.querySelectorAll('#' + book + ' input[type=checkbox]');
+    var b = books[book];
+    for (var i = 0; i < a.length; i++) {
+        b[i] = a[i].checked;
+        if (a[i].checked) {
             count++;
         }
-    });
-    books[book] = count;
+    }
+
+    setBookPercentage(book, count);
+    setBiblePercentage();
+
+    idbKeyval.set('books', books);
+}
+
+function setBiblePercentage() {
+    var totalChapters = 1189;
     var completedChapters = 0;
     for (var name in books) {
-        completedChapters += books[name];
+        completedChapters += books[name].filter(chapterStatus => chapterStatus).length;
     }
     document.querySelector('#complete').textContent =
         Math.floor((completedChapters / totalChapters) * 100) + '%';
-    var percentage = Math.floor((count / chapters) * 100);
+}
+
+function setBookPercentage(book, count) {
+    var percentage = Math.floor((count / books[book].length) * 100);
     document.querySelector('#' + book + '-badge').setAttribute('data-badge', percentage + '%');
+}
+
+function initBooks() {
+    return {
+        genesis: new Array(50),
+        exodus: new Array(40),
+        leviticus: new Array(27),
+        numbers: new Array(36),
+        deuteronomy: new Array(34),
+        joshua: new Array(24),
+        judges: new Array(21),
+        ruth: new Array(4),
+        firstsamuel: new Array(31),
+        secondsamuel: new Array(24),
+        firstkings: new Array(22),
+        secondkings: new Array(25),
+        firstchronicles: new Array(29),
+        secondchronicles: new Array(36),
+        ezra: new Array(10),
+        nehemiah: new Array(13),
+        esther: new Array(10),
+        job: new Array(42),
+        psalms: new Array(150),
+        proverbs: new Array(31),
+        ecclesiastes: new Array(12),
+        song: new Array(8),
+        isaiah: new Array(66),
+        jeremiah: new Array(52),
+        lamentations: new Array(5),
+        ezekiel: new Array(48),
+        daniel: new Array(12),
+        hosea: new Array(14),
+        joel: new Array(3),
+        amos: new Array(9),
+        obadiah: new Array(1),
+        jonah: new Array(4),
+        micah: new Array(7),
+        nahum: new Array(3),
+        habakkuk: new Array(3),
+        zephaniah: new Array(3),
+        haggai: new Array(2),
+        zechariah: new Array(14),
+        malachi: new Array(4),
+        matthew: new Array(28),
+        mark: new Array(16),
+        luke: new Array(24),
+        john: new Array(21),
+        acts: new Array(28),
+        romans: new Array(16),
+        firstcorinthians: new Array(16),
+        secondcorinthians: new Array(13),
+        galatians: new Array(6),
+        ephesians: new Array(6),
+        philippians: new Array(4),
+        colossians: new Array(4),
+        firstthessalonians: new Array(5),
+        secondthessalonians: new Array(3),
+        firsttimothy: new Array(6),
+        secondtimothy: new Array(4),
+        titus: new Array(3),
+        philemon: new Array(1),
+        hebrews: new Array(13),
+        james: new Array(5),
+        firstpeter: new Array(5),
+        secondpeter: new Array(3),
+        firstjohn: new Array(5),
+        secondjohn: new Array(1),
+        thirdjohn: new Array(1),
+        jude: new Array(1),
+        revelation: new Array(22)
+    };
 }
